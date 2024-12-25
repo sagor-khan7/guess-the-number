7158f00943934009c2576b881cdb17b71e8727bd
sagor-khan7 <sksagor791@gmail.com>
2024-12-25 21:14:03 +0600

    Adding functionality to the Again button for resetting all the initial value

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
@@ -17,6 +16,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🎉 Correct number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -38,4 +38,29 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+/**
+ *todo: Coding challenge: 
+ Implement a game reset functionality, so that the player can make a new guess, and here's how :
+
+ 1. Select the element with "again" class and attach a event handler to it. ✅
+ 2. In the handler function reset initial values of the "score" and the "secretNumber" variable ✅
+ 3. Restore all initial conditions of the message, number, score, and guess input field.
+ 4. Also restore the original background color (#222) and number width (15rem)
+*/
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = secretNumber;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
